5cc5cd91c62ee6cb81a5d11c7dc8248ee4e136cc
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:01:25 +0530

    Make render-build.sh executable for Render build

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from app.routes import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Allow external connections by setting host to '0.0.0.0'
+    app.run(debug=True, host='0.0.0.0', port=5000)
