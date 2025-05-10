407d2f6bcce2c0b51031ea2c4d3d5f61d71c35e2
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:09:56 +0530

    skip installation step

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y chromium-browser chromium-chromedriver
+# Remove Chromium installation step as it's pre-installed on Render
+# apt-get update && apt-get install -y chromium-browser chromium-chromedriver
