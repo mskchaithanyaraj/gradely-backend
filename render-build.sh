bda9d3d92a93fec0fb7863f5bf972e91f3e06614
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:59:56 +0530

    updated render build file

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Install dependencies listed in requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt  # Ensure this includes selenium and undetected-chromedriver
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y wget gnupg2 unzip
