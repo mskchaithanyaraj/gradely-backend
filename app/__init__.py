0dd0c9ca28053b568fc05f15346984d3bb02193d
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:38:15 +0530

    update cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # Enable CORS for your frontend
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5173", "https://gradely-srgec.netlify.app"]}})
 
 # Import routes
 from . import routes
