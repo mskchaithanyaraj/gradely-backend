9e71c9436b8fec0c52b15e6695b4165c31cd3998
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:55:26 +0530

    configurec manual chrome installation in render

--- app/scraper.py
+++ app/scraper.py
@@ -19,9 +19,9 @@ def login_and_fetch_data(username, password):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
-    options.binary_location = os.getenv("GOOGLE_CHROME_BIN", "/opt/render/project/.apt/usr/bin/google-chrome")
+    options.binary_location = "/usr/bin/google-chrome"
 
-    print("Chrome binary path:", os.path.exists(options.binary_location))
+    print("Chrome binary path:", os.path.exists("/usr/bin/google-chrome"))
 
 
     driver = uc.Chrome(options=options)
