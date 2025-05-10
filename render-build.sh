0e897677b3cae0d324716a05bf95e7eb90c35afb
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 17:58:37 +0530

    updated build file

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-# Install dependencies listed in requirements.txt
-pip install --upgrade pip
-pip install -r requirements.txt  # Ensure this includes selenium and undetected-chromedriver
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y wget gnupg2 unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
