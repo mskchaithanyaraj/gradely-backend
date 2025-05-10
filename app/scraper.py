55936c068ea673e8deb59a52ac8f8bf5998309ae
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-10 18:51:48 +0530

    update scraper file

--- app/scraper.py
+++ app/scraper.py
@@ -31,7 +31,7 @@ def login_and_fetch_data(username, password):
     driver = None
 
     try:
-        driver = uc.Chrome(version_main=117, options=options, browser_executable_path=CHROME_BIN_PATH)
+        driver = uc.Chrome(options=options, browser_executable_path=CHROME_BIN_PATH)
         logging.info("Chrome driver launched successfully.")
 
         driver.get("https://gecgudlavalleruonlinepayments.com/")
